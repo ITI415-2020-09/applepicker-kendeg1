f323c7e37eeb1ad311495c02bfc22e46edbec1b2
kendeg1 <kendeg1@gmail.com>
2020-09-18 16:20:04 -0400

    baskets destroyed
    added public function to apple picker to be called by basket script that destroys all apples  and lose 1 basket if an apple does not collide with a basket

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Apple : MonoBehaviour
 {
 
+    [Header("Set in Inspector")]
+
     public static float bottomY = -20f;
 
     // Start is called before the first frame update
@@ -20,6 +22,14 @@ public class Apple : MonoBehaviour
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+
+            //get a reference to the applepicker component of main camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+
+            //call the public AppleDestroyed() method of apScript
+
+            apScript.AppleDestroyed();
+
         }
 
     }
--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -11,10 +12,15 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
 
     // Start is called before the first frame update
     void Start()
     {
+
+        basketList = new List<GameObject>();
+
         for (int i=0; i<numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
@@ -22,7 +28,33 @@ public class ApplePicker : MonoBehaviour
 
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+
+        foreach(GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        //destroy one of the baskets
+        //get the index of the last basket in the basketList
+
+        int basketIndex = basketList.Count - 1;
+
+        //get a reference to that Basket GameObject
+
+        GameObject tBasketGO = basketList[basketIndex];
+
+        //remove the basket from the list and destroy the GameObject
+
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+
     }
 
     // Update is called once per frame
