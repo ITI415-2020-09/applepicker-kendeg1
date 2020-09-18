7c41180c7b7fc87a8273263b856c3a0d690a2628
kendeg1 <kendeg1@gmail.com>
2020-09-18 15:18:15 -0400

    basket changes
    changed basket so that if game object with tag == apple collides with game object basket, the apple is destroyed.

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript2 : MonoBehaviour
+public class Basket : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,31 @@ public class NewBehaviourScript2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Get the current screen position of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //the camera's z position sets how far to push the mouse into 3d
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //move the x position of this basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
+
 }
