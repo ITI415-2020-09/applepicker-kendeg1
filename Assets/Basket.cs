bba222332b6c18de6f6ca3ea77594e741a46933a
kendeg1 <kendeg1@gmail.com>
2020-09-18 16:01:37 -0400

    scoring
    added high score and score counter to scene

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        //Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        //Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        //Set the starting number of points to 0
+        scoreGT.text = "0";
+
     }
 
     // Update is called once per frame
@@ -38,6 +52,14 @@ public class Basket : MonoBehaviour
         {
             Destroy(collidedWith);
         }
+
+        //Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+        //add points for catching the apple
+        score += 100;
+
+        scoreGT.text = score.ToString();
+
     }
 
 }
